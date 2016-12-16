d69b01dccdf86c254a7110627fe1a2bba5efb730
Mike Charles <mike.charles@noaa.gov>
2016-12-16 17:44:08 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ requirements = [
 
 setup(
     name="cpc.mpp",
-    version='v0.1.0',
+    version='v0.1.1',
     description="Model post processing package",
     long_description=readme + '\n\n' + history,
     author="Mike Charles",
